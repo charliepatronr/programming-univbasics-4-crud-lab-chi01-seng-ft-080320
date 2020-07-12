e82f59b595c7c6f943356545b0b6c227d1704fbf
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-12 19:52:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ['charlie', 'paola', 'patron', 'portilla']
+  arr =['charlie', 'paola', 'patron', 'portilla']
 end
 
 def add_element_to_end_of_array(array, element)
